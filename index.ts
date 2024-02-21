d2c47e63126a6e62dc4cf9c14489a39969dc38cf
Brandon Sturgeon <brandon@brandonsturgeon.com>
2024-02-21 00:54:16 -0800

    Fetch stuff again...

--- index.ts
+++ index.ts
@@ -5,7 +5,6 @@ const ManageAddons = require("wisp_addon_manager").ManageAddons;
 const nodeFetch = require("node-fetch");
 declare global {
   var Headers: typeof nodeFetch.Headers;
-  var fetch: typeof nodeFetch.default;
 }
 globalThis.Headers = nodeFetch.Headers;
 globalThis.fetch = nodeFetch.default || nodeFetch;
